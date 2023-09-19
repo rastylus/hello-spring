3c1f44b7cf3b313f1d0157fa652e5cf0c22ae954
rastylus <ryan.p.riggs@gmail.com>
2023-09-19 16:40:15 -0500

    added class annotations

--- src/main/java/com/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/com/launchcode/hellospring/controllers/HelloController.java
@@ -1,37 +1,49 @@
 package com.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
     //handles request at path /hello instead of root 8080
 //    @GetMapping("hello")
-//    @ResponseBody
 //    public String hello() {
 //        return "Hello, Spring!";
 //    }
+
+
 //    // responds to get requests at /hello?coder=Launchcoder
-    @GetMapping("hello")
-    @ResponseBody
-    public String helloRequest(@RequestParam String coder) {
-        return "Hello, " + coder + "!";
+//    @GetMapping("hello")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    @GetMapping("{name}")
         public String helloAgain(@PathVariable String name){
             return "Hello, " + name + "!";
     }
 
-    @GetMapping("goodbye")
-    @ResponseBody
+    @GetMapping("goodbye") //now lives at hello/goodbye after putting Request
+    // mapping above controller class
     public String goodbye() {
         return "Goodbye, Spring!";
+    }
+
+    @GetMapping("form") //    /hello/form
+    public String helloForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action= 'hello' method='post'>" + //submits a request to
+                // /hello
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
     }
 
 
